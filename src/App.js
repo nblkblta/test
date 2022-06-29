4b55bbc26e4cd1b7a8828e796ef2b9d5474f2efc
nblkblta <1a2w3e4R_>
2022-06-29 16:33:21 +1000

    fix bugs, add pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
 import PostsBody from "./Components/PostsBody";
 import AddForm from "./Components/AddForm";
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState(``);
+    const [posts, setPosts] = useState([]);
     const [isLoading, setLoading] = useState(true)
+    const [totalPosts, setTotalPosts] = useState(0)
+    const [postsOnPageLimit, setPostsOnPageLimit] = useState(10)
+    const [currentPage, setCurrentPage] = useState(1)
     const fetchPosts = () =>{
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => setPosts(response.data))
-            .then(setLoading(false))
+         axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {setPosts(response.data);setTotalPosts(response.data.length)})
+            .then(setTimeout(load, 300))
             .catch(error=>alert(error.message))
     }
 
+    const load = () => {
+        setLoading(false);
+    }
     useEffect(()=>{
         setLoading(true)
-
-        setTimeout(fetchPosts,1000)
+        fetchPosts()
     }, [])
 
     const delPost= (id)=>{
         setPosts(posts.filter(e=> e.id !== id));
+        setTotalPosts(prev => prev -1)
     }
 
     const addPost = (post)=>{
@@ -30,12 +36,12 @@ function App() {
 
     return (
         <div>
+            <input value={currentPage} onChange={event => setCurrentPage(event.target.value)}/>
+            <input value={postsOnPageLimit} onChange={event => setPostsOnPageLimit(event.target.value)}/>
             <AddForm addPost={addPost}/>
-            {isLoading ? <div>Загрузка</div> : posts.length !== 0
-                                ?  <PostsBody value={posts} del={delPost}/>
-                                :  <div>Нет постов</div>
-            }
-
+            {isLoading ? <div>Загрузка</div> : <PostsBody value={posts.slice((currentPage-1)*postsOnPageLimit,(currentPage-1)*postsOnPageLimit+postsOnPageLimit)} del={delPost}/>}
+            {posts.length === 0 && !isLoading ? <div>Нет постов</div> : null}
+            <div>Всего постов: {totalPosts}</div>
         </div>
 
     );
