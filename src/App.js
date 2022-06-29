4fb1bbbd99dbd261931a50442eaaa6cea6e90727
nblkblta <nblkdjum@gmail.com>
2022-06-29 13:51:58 +1000

    add loader

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import './App.css';
 import PostsBody from "./Components/PostsBody";
 import AddForm from "./Components/AddForm";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
-    const [posts, setPosts] = useState([{id: 1, title: `Заголовок 1`, body: 'Описание 1'},
-                 {id: 2, title: `Заголовок 2`, body: 'Описание 2'},
-                 {id: 3, title: `Заголовок 3`, body: 'Описание 3'},
-                 {id: 4, title: `Заголовок 4`, body: 'Описание 4'},
-                 {id: 5, title: `Заголовок 5`, body: 'Описание 5'},
-                 {id: 6, title: `Заголовок 6`, body: 'Описание 6'},
-                 {id: 7, title: `Заголовок 7`, body: 'Описание 7'},
-                 {id: 8, title: `Заголовок 8`, body: 'Описание 8'}
-    ]);
+    const [posts, setPosts] = useState(``);
+    const [isLoading, setLoading] = useState(true)
+    const fetchPosts = () =>{
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => setPosts(response.data))
+            .then(setLoading(false))
+            .catch(error=>alert(error.message))
+    }
+
+    useEffect(()=>{
+        setLoading(true)
+
+        setTimeout(fetchPosts,1000)
+    }, [])
+
     const delPost= (id)=>{
         setPosts(posts.filter(e=> e.id !== id));
     }
+
     const addPost = (post)=>{
         setPosts([...posts,{id: Date.now(), ...post}])
     }
+
     return (
         <div>
             <AddForm addPost={addPost}/>
-            {posts.length !== 0
+            {isLoading ? <div>Загрузка</div> : posts.length !== 0
                                 ?  <PostsBody value={posts} del={delPost}/>
-                                :  <div>Загрузка</div>
+                                :  <div>Нет постов</div>
             }
 
         </div>
